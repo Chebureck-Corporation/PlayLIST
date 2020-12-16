1b324eac298c0b331a3cde04f7f489a8a9458d37
thethirtyfourth <nur-14@list.ru>
2020-12-16 14:25:41 +0300

    fixed params

--- app/src/main/java/com/chebureck/playlist/db/DBAccessor.kt
+++ app/src/main/java/com/chebureck/playlist/db/DBAccessor.kt
@@ -1,12 +1,11 @@
 package com.chebureck.playlist.db
 
-import com.chebureck.playlist.PlaylistApplication
 import com.chebureck.playlist.operations.Operations
 
-object DBAccessor {
-    private val operation = Operations()
-    private val playlistDao: PlaylistDaoImpl = PlaylistApplication().playlistDao
-
+class DBAccessor(
+    private val operation: Operations,
+    private val playlistDao: PlaylistDaoImpl
+) {
     fun and(playlist: Playlist, tracksList: List<List<Track>>) {
         val result = operation.and(tracksList)
         playlistDao.addPlaylist(playlist)
