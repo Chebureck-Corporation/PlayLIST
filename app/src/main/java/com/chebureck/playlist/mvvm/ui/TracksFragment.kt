2cfd4ec6fa237df8e599b8fdde9fbe96dc270f6c
washinson <sanya112001@yandex.ru>
2020-12-23 14:16:32 +0300

    Playlist push temp button created

--- app/src/main/java/com/chebureck/playlist/mvvm/ui/TracksFragment.kt
+++ app/src/main/java/com/chebureck/playlist/mvvm/ui/TracksFragment.kt
@@ -1,33 +1,51 @@
 package com.chebureck.playlist.mvvm.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chebureck.playlist.R
-import com.chebureck.playlist.db.Track
+import com.chebureck.playlist.databinding.FragmentTracksScreenBinding
 import com.chebureck.playlist.mvvm.ui.widget.tracks.TracksAdapter
 import com.chebureck.playlist.mvvm.ui.widget.tracks.TracksAdapterItem
+import com.chebureck.playlist.mvvm.viewmodel.SelectedPlaylistViewModel
+import com.chebureck.playlist.mvvm.viewmodel.SpotifyViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import org.koin.core.component.KoinApiExtension
 
+@KoinApiExtension
 class TracksFragment : Fragment(R.layout.fragment_tracks_screen), TracksAdapter.TrackClickListener {
+
+    private val selectedPlaylistViewModel
+            by sharedViewModel<SelectedPlaylistViewModel>()
+    private val spotifyViewModel by sharedViewModel<SpotifyViewModel>()
+
     private val tracksAdapter = TracksAdapter(this)
     private lateinit var tracks: RecyclerView
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        @Suppress("UNCHECKED_CAST")
-        val tracks = arguments?.getParcelableArray(BUNDLE_NAME_TRACKS) as Array<Track>
-        tracksAdapter.tracks = tracks.toList().map { TracksAdapterItem(it, false) }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        selectedPlaylistViewModel.getSelectedPlaylist().observe(viewLifecycleOwner) { playlist ->
+            tracksAdapter.tracks = playlist.tracks.map { TracksAdapterItem(it, false) }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tracks = view.findViewById(R.id.recycler_tracks)
+        val binding = FragmentTracksScreenBinding.bind(view)
+
+        tracks = binding.recyclerTracks
         tracks.layoutManager = LinearLayoutManager(context)
         tracks.adapter = tracksAdapter
+
+        val options = binding.btnOptions
+        options.setOnClickListener {
+            selectedPlaylistViewModel.getSelectedPlaylist().value?.let { playlist ->
+                spotifyViewModel.createSpotifyPlaylist(playlist)
+            }
+        }
     }
 
     override fun onTrackClicked(position: Int) {
